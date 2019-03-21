07899cd27805d45328f25b5950df412a9b4d7d76
Antoine <ahoorelbeke@gmail.com>
2019-03-21 21:51:40 +0100

    Add constraint in simplex nmf Minor fixes

--- simplex_nmf.py
+++ simplex_nmf.py
@@ -8,11 +8,17 @@ class SimplexNMF():
             self.f = f
             self.n = n
             F = np.random.random((f, r))
+            F = SimplexNMF.norm_rows_to_one(F)
             W = np.random.random((r, n))
+            W = SimplexNMF.norm_rows_to_one(W)
             self.X = F @ W
         else:
             self.f, self.n = X.shape
             self.X = X
+
+    @staticmethod
+    def norm_rows_to_one(matrix):
+        return matrix / np.expand_dims(np.sum(matrix, axis=1), -1)
 
     def build_a_eq(self):
         A = np.zeros((self.f*self.n + 1, self.f**2))
@@ -32,7 +38,7 @@ class SimplexNMF():
         self.b_eq = b
 
     def build_a_ub(self):
-        A = np.zeros((self.f**2, self.f**2))
+        A = np.zeros((2*self.f**2, self.f**2))
         index_A = 0
 
         # Handle diag(C) <= 1
@@ -50,11 +56,18 @@ class SimplexNMF():
                 C[i, j] = 1
                 A[index_A, :] = C.flatten()
                 index_A += 1
+
+        # Handle -C_ij <= 0
+        for i in range(self.f):
+            for j in range(self.f):
+                A[index_A, i*self.f + j] = -1
+                index_A += 1
+        
         self.A_ub = A
 
     def build_b_ub(self):
-        # Handle C_ij - C_jj <= 0  
-        b = np.zeros(self.f**2)
+        # Handle C_ij - C_jj <= 0 and -C_ij <= 0
+        b = np.zeros(2*self.f**2)
         # Handle diag(C) <= 1
         b[:self.f] += 1
         self.b_ub = b
@@ -70,8 +83,16 @@ class SimplexNMF():
         self.build_t()
         self.result = linprog(self.t, method='simplex', A_eq=self.A_eq, b_eq=self.b_eq, A_ub=self.A_ub, b_ub=self.b_ub)
         self.c = self.result.x.reshape(self.f, self.f)
+        self.build_nmf()
         self.check_result()
+
+    def build_nmf(self):
+        dia = np.diag(self.c)
+        index = np.argpartition(dia, -self.r)[-self.r:]
+        self.W = self.X[index, :]
+        self.F = self.c[:, index]
 
     def check_result(self):
         print('||CX - X|| = {}'.format(np.sum(np.square(self.c @ self.X - self.X))))
         print('diag(C) = {}'.format(np.diag(self.c)))
+        print('||FW - X|| = {}'.format(np.sum(np.square(self.F @ self.W - self.X))))
